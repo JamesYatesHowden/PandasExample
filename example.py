76c8487a41c9f40ec6ac13ac5022ac66a659453f
yatesjam <james.yates@howden.com>
2022-08-31 15:03:24 +0100

    init

---
+++ example.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+def example():
+    inputs = pd.read_csv('inputs.csv')
+    stats = inputs.describe(percentiles=[0.05, 0.9]) 
+    stats.to_csv('stats.csv')
+
+
+if __name__ == "__main__":
+    example()
