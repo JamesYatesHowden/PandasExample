cb2b11deb8be4c834fabd21d238068f7f8f34709
yatesjam <james.yates@howden.com>
2022-08-31 15:05:44 +0100

    add print

--- example.py
+++ example.py
@@ -4,6 +4,7 @@ import pandas as pd
 def example():
     inputs = pd.read_csv('inputs.csv')
     stats = inputs.describe(percentiles=[0.05, 0.9]) 
+    print(f'\n\n {stats} \n\n')
     stats.to_csv('stats.csv')
 
 
